1c0eac3d2a1ba87a15634713347a54cc9d5910e3
Luis Rodríguez <lfrz.arc@gmail.com>
2024-12-11 17:30:28 -0600

    document component created por writings

--- src/components/pages/writings/Writings.tsx
+++ src/components/pages/writings/Writings.tsx
@@ -1,3 +1,6 @@
+import './writings.css'
+
+
 interface DocumentProps {
   title: string
   content: string
@@ -7,7 +10,12 @@ const Document: React.FC<DocumentProps> = ({ title, content }) => {
   return (
     <div className="document_container">
       <h2>{title}</h2>
-      <p>{content}</p>
+      <div className="content_container">
+        <p>{content}</p>
+      </div>
+      <p className="keep_reading">
+        Seguir leyendo
+      </p>
     </div>
   )
 }
@@ -15,11 +23,12 @@ const Document: React.FC<DocumentProps> = ({ title, content }) => {
 const Writings = () => {
   const DOCUMENT = {
     title: 'Mi separación de la religión',
-    content: 'Hace mucho tiempo dejé de creer en Dios, no sin reemplazar las cosas que ofrece tener una religión. Aquí comparto las respuestas que he encontrado a través de la razón a los problemas que la religión intenta resolver'
+    content: 'Hace mucho tiempo dejé de creer en Dios, no sin reemplazar las cosas que ofrece tener una religión. Aquí comparto las respuestas que he encontrado a través de la razón, la ciencia y el propósito de mejorar como individuo a los problemas que la religión intenta resolver como son el sentido de la vida y la explicación del origen de las cosas'
   }
 
   return (
     <div className="writings_container">
+      <Document title={DOCUMENT.title} content={DOCUMENT.content} />
       <Document title={DOCUMENT.title} content={DOCUMENT.content} />
     </div>
   )
